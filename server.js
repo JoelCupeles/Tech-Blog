5399016cf6cacf022ce7978ba02d72555dc616e0
JoelCupeles <joel.cupeles1998@gmail.com>
2023-05-10 12:55:53 -0500

    Server.js setup, modules installation.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 30 * 60 * 1000 }, 
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sessionConfig));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
+
