e49ea42f76dfe7be87c55e5b21968056c2076cb4
JoelCupeles <joel.cupeles1998@gmail.com>
2023-07-02 15:45:45 -0500

    Models setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./src/controllers');
+const sequelize = require('./src/config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
